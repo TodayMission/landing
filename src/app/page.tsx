257ffd4c3dc6f8d770a18bdf1e6bd644b18f4add
stefan <dupontstef80@gmail.com>
2026-05-14 16:30:17 +0200

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,100 @@
 import Image from "next/image";
+import PrimaryButton from "./components/button";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main>
+      <div className="sm:grid grid-cols-2 grid-rows-1 overflow-hidden">
+        <div className="p-7 md:flex md:justify-center md:flex-col">
+          <div className="flex justify-center">
+            <h2 className="max-w-xs text-center text-3xl font-bold">Défiez vos amis sans contraintes</h2>
+          </div>
+          <div className="m-5">
+            <p className="text-lg">Today’s mission vous permet d’élaborer des groupes pour faire des challenges entre amis sans prises de têtes</p>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="md:pbs-20">
+          <div className="flex justify-center rotate-5 content-center"><img src="/captures.png" alt="photo de l'application" width={300}/></div>
         </div>
-      </main>
-    </div>
+        <div className="flex justify-evenly p-10 lg:p-0 items-center lg:overflow-visible lg:-mbs-50">
+          <img src="/playstore.png" alt="Download playstore" width={200}/>
+          <PrimaryButton text="Télécharger"></PrimaryButton>
+        </div>
+      </div>
+      <div className="">
+        <div className="flex justify-center flex-col items-center">
+          <h2 className="pbs-10 pbe-5 max-w-xs lg:max-w-max  text-center text-3xl font-bold">Créer des missions simplement</h2>
+          
+          <div className="lg:flex lg:flex-row">
+            <div>
+              <div className="p-5">
+                <div className="flex lg:flex-col gap-20 pbs-20 items-center justify-center">
+                  <div className="min-w-30 min-h-30 bg-[#E3EBF0] flex justify-center items-center rounded-full"><span className="text-5xl">1</span></div>
+                  <h2 className="text-2xl font-semibold text-center">Embarquez vos amis</h2>
+                </div>
+                <div>
+                  <p className="p-5">Partgez un lien d’invitation avec votre ami, une fois que votre proche à créer son compte, il apparait directement dans votre liste d’ami.</p>
+                </div>
+              </div>
+
+            </div>
+
+            <div>
+              <div className="p-5">
+                <div className="flex lg:flex-col-reverse gap-20 pbs-20 items-center justify-center">
+                  <h2 className="text-2xl font-semibold text-center">Créez un groupe avec vos amis</h2>
+                  <div className="min-w-30 min-h-30 bg-[#E3EBF0] flex justify-center items-center rounded-full"><span className="text-5xl">2</span></div>
+                </div>
+                <div>
+                  <p className="p-5">Créez votre groupe en vous dirigeant dans la page groupe. Invitez vos amis dans votre groupes et partagez des moments amusants.</p>
+                </div>
+              </div>
+            </div>
+          
+            <div>
+
+              <div className="p-5">
+                <div className="flex lg:flex-col gap-20 pbs-20 items-center justify-center">
+                  <div className="min-w-30 min-h-30 bg-[#E3EBF0] flex justify-center items-center rounded-full"><span className="text-5xl">3</span></div>
+                  <h2 className="text-2xl font-semibold text-center">Inventez des challenges</h2>
+                </div>
+                <div>
+                  <p className="p-5">Maintenant que vous êtes dans votre groupe avec vos amis, imaginez les défis les plus improbable et voyez qui osera les relever.</p>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+        <div className="pbe-10">
+          <div className="flex justify-center flex-col items-center ">
+            <h2 className="pbs-10 pbe-5 max-w-xs lg:max-w-max  text-center text-3xl font-bold">Une liberté d'adaptation</h2>
+            <p className="p-5">Plus qu’une simple application de défi, vous pouvez challengez votre quotidien en toute circonstances.</p>
+          </div>
+          <div className="flex lg:flex-row flex-col sm:gap-10">
+            <div className="p-3 m-5 pbe-10 bg-[#F0E8E3] rounded-2xl shadow-xl">
+              <div className="flex p-5 gap-5 items-center">
+                <div className="flex bg-white rounded-full min-w-20 min-h-20 justify-center items-center"><HomeOutlinedIcon style={{ fontSize: 40 }}></HomeOutlinedIcon></div>
+                <h2>EN FAMILLE</h2>
+              </div>
+              <p>Partagez des challenges sur les tâches ménagéres ou sur la gestion autonome des différents membre de famille.  Ainsi que des petits défis amusant pour stimuler l’ensemble des membres de la famille avec des activités communes.</p>
+            </div>
+
+            <div className="p-3 m-5 pbe-10 bg-[#F0E8E3] max-w-(80vw) rounded-2xl shadow-xl">
+              <div className="flex p-5 gap-5 items-center">
+                <div className="flex bg-white rounded-full min-w-20 min-h-20 justify-center items-center"><GroupsOutlinedIcon style={{ fontSize: 40 }}></GroupsOutlinedIcon></div>
+                <h2>ENTRE AMIS</h2>
+              </div>
+              <p>Que ça soit loufoques ou accès sur vos passions communes vous pouvez vous ammenez à découvrir de nouveaux centre d’intêret commun ou à renforcer vos liens en surmontants vos différents challenges.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </main>
   );
 }
