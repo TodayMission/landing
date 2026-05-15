2e2383b144d34e2c17227de536f949f05c05cdf9
stefan <dupontstef80@gmail.com>
2026-05-15 09:22:28 +0200

    fix(metadata): typo in file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "Today's mission",
   description: "Essayer l'application today's mission pour créer des groupes et des challenges avec vos amis",
   openGraph: {
-    images: ["/og_original.png"],
+    images: ["/oc_original.png"],
   },
 };
 
